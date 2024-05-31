326f3c6666c82d2cd6c72ba6a4a9d4828c429363
Nicolas Bellesi <nicolas.crbe@gmail.com>
2024-05-30 22:04:04 -0300

    caso no exitoso

--- src/acceptanceTest/java/ar/com/grupoesfera/repartir/steps/gastos/GastoNoNegativoSteps.java
+++ src/acceptanceTest/java/ar/com/grupoesfera/repartir/steps/gastos/GastoNoNegativoSteps.java
@@ -5,20 +5,23 @@ import io.cucumber.java.es.Entonces;
 import io.cucumber.java.es.Y;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElement;
-import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
 public class GastoNoNegativoSteps extends CucumberSteps {
 
     @Y("el usuario agrega un gasto de ${string} al grupo")
-    public void elUsuarioAgregaUnGastoDe$AlGrupo(String gasto) {
+    public void elUsuarioAgregaUnGastoDeAlGrupo(String gasto) {
+        WebDriverWait wait = new WebDriverWait(driver, 10); // Incrementar tiempo de espera
 
-        var agregarGasto = driver.findElement(By.id("agregarGastoGruposButton-1"));
-        agregarGasto.click();
-        var input = driver.findElement(By.id("montoGastoNuevoInput"));
+        WebElement buttonAgregarGasto = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("agregarGastoGruposButton-1")));
+        buttonAgregarGasto.click();
+
+        WebElement input = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("montoGastoNuevoInput")));
+
         input.click();
         input.sendKeys(Keys.BACK_SPACE);
         input.sendKeys(Keys.BACK_SPACE);
@@ -26,24 +29,17 @@ public class GastoNoNegativoSteps extends CucumberSteps {
         input.sendKeys(Keys.BACK_SPACE);
 
         input.sendKeys(gasto);
-        driver.findElement(By.id("guardarGastoNuevoButton")).click();
-    }
-
-    @Entonces("debería visualizar el grupo con total ${string}")
-    public void deberiaVisualizarElGrupoConTotal(String total) {
-        var grupoTD = driver.findElements(By.cssSelector("app-grupos p-card p-table tr"))
-                .get(1).findElements(By.tagName("td")).get(2).getText();
-
-        assertThat(grupoTD).contains(total);
+        WebElement button = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("guardarGastoNuevoButton")));
+        button.click();
     }
 
     @Entonces("se muestra un mensaje de error indicando que el gasto no puede ser negativo")
     public void seMuestraUnMensajeDeErrorIndicandoQueElGastoNoPuedeSerNegativo() {
-        var wait = new WebDriverWait(driver, 2);
-        var mensajesToast = wait.withMessage("Mostro Toast")
-                .until(visibilityOfElementLocated(By.id("mensajesToast")));
+        WebDriverWait wait = new WebDriverWait(driver, 2);
+        WebElement mensajesToast = wait.withMessage("Mostro Toast")
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("mensajesToast")));
         wait.withMessage("Título del Toast es 'Error'")
-                .until(textToBePresentInElement(mensajesToast, "Error"));
+                .until(ExpectedConditions.textToBePresentInElement(mensajesToast, "Error"));
         assertThat(mensajesToast.getText())
                 .as("Descripción del Toast")
                 .contains("No se puede guardar");
